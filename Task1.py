f256901a7a6f069bc6b1c1d0226bb632e45115cc
Artyom Khmelyov <khmelyov-a@yandex.ru>
2015-10-16 19:10:42 +0300

    Task1

--- Task1.py
+++ Task1.py
@@ -1,2 +1,10 @@
 __author__ = 'artyom'
-цукдоцута
+Result = 0
+for i in range(1000):
+    if i % 15 == 0:
+        Result += i
+    elif i % 3 == 0:
+        Result += i
+    elif i % 5 == 0:
+        Result += i
+print(Result)
