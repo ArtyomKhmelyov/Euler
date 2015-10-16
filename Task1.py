49827db835945bbc00ddaea364f03c9d4e1439dc
Artyom Khmelyov <khmelyov-a@yandex.ru>
2015-10-16 23:42:24 +0300

    Task1

--- Task1.py
+++ Task1.py
@@ -1,10 +1,6 @@
 __author__ = 'artyom'
 Result = 0
 for i in range(1000):
-    if i % 15 == 0:
-        Result += i
-    elif i % 3 == 0:
-        Result += i
-    elif i % 5 == 0:
+    if i % 3 == 0 or i % 5 ==0:
         Result += i
 print(Result)
