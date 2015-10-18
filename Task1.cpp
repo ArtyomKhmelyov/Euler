15aa3eba44af91277b534cf6170fb56464456ea9
Artyom Khmelyov <khmelyov-a@yandex.ru>
2015-10-18 11:07:42 +0300

    Task1 c++

--- Task1.cpp
+++ Task1.cpp
@@ -3,3 +3,16 @@
 #include <iostream>
 
 using namespace std;
+
+int main()
+{
+   int Result = 0;
+    for (int i=1;i<1000;i++)
+    {
+        if ((i % 3 == 0) or (i % 5 == 0))
+        {
+            Result += i;
+        }
+    }
+    cout<< Result;
+}
