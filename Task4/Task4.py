e17ce4ecedcbdd594fcb85c77d5ae14beee93061
Artyom Khmelyov <khmelyov-a@yandex.ru>
2015-10-19 22:46:52 +0300

    Task4

--- Task4/Task4.py
+++ Task4/Task4.py
@@ -1,1 +1,14 @@
-__author__ = 'artyom'
+def isPalindrome(n):
+    if str(n)[::-1] == str(n):
+        return True
+    else:
+        return False
+
+Result = 0
+for i in range(100, 999):
+    for j in range(100, 999):
+        if isPalindrome(i*j):
+            if i*j > Result:
+                Result = i*j
+
+print Result
