eefb4d4160b3e9db90d0d03938c6d612966e6c8d
Artyom Khmelyov <khmelyov-a@yandex.ru>
2015-10-19 22:33:32 +0300

    Task3

--- Task3/Task3.py
+++ Task3/Task3.py
@@ -1,1 +1,21 @@
-__author__ = 'artyom'
+import math
+const = 600851475143
+Result = 0
+
+def IsPrime(n):
+    if n % 2 == 0:
+        return False
+    else:
+        for i in range(3, int(math.sqrt(n))):
+            if n % i == 0:
+                return False
+    return True
+
+i = 1
+while i < math.sqrt(const):
+    if IsPrime(i) and const % i == 0:
+        Result = i
+    i += 1
+
+print Result
+
