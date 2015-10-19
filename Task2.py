2c76927b293f72ed207a88f653d11366a9854572
Artyom Khmelyov <khmelyov-a@yandex.ru>
2015-10-19 22:04:12 +0300

    Task2 done

--- Task2.py
+++ Task2.py
@@ -1,1 +1,20 @@
-__author__ = 'artyom'
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == -1 or n == 1:
+        return 1
+    else:
+        if n > 0:
+            return fib(n-1) + fib(n-2)
+        else:
+            return fib(n+2) - fib(n+1)
+
+
+Result = 0
+for i in range(60):
+    if fib(i) > 4000000:
+        break
+    if fib(i) % 2 == 0:
+        Result += fib(i)
+print Result
+
